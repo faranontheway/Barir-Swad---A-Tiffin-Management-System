c907294d9ae518c76be3c01f70c7b99f272f4889
mourinpixels <mehjabin.hasan01@gmail.com>
2025-09-02 16:53:26 +0600

    Update barirswad.sql

--- barirswad.sql
+++ barirswad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 01, 2025 at 07:02 PM
+-- Generation Time: Sep 02, 2025 at 12:47 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,14 +39,16 @@ CREATE TABLE `complaint_support` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer_rates_cook`
+-- Table structure for table `customer_rates_cooks`
 --
 
-CREATE TABLE `customer_rates_cook` (
-  `Customer_ID` int(10) NOT NULL,
-  `Cook_ID` int(10) NOT NULL,
-  `Rating` decimal(3,2) NOT NULL,
-  `Comment` text NOT NULL
+CREATE TABLE `customer_rates_cooks` (
+  `ReviewID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `CookID` int(11) NOT NULL,
+  `Rating` decimal(2,1) DEFAULT NULL CHECK (`Rating` >= 1 and `Rating` <= 5),
+  `Comment` text DEFAULT NULL,
+  `Created_At` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -60,7 +62,7 @@ CREATE TABLE `meal` (
   `Name` varchar(100) NOT NULL,
   `Description` text NOT NULL,
   `Proportion` varchar(50) NOT NULL,
-  `Pricing List` decimal(10,2) NOT NULL,
+  `Pricing` decimal(10,2) NOT NULL,
   `Cuisine` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -68,7 +70,7 @@ CREATE TABLE `meal` (
 -- Dumping data for table `meal`
 --
 
-INSERT INTO `meal` (`Meal_ID`, `Name`, `Description`, `Proportion`, `Pricing List`, `Cuisine`) VALUES
+INSERT INTO `meal` (`Meal_ID`, `Name`, `Description`, `Proportion`, `Pricing`, `Cuisine`) VALUES
 (1, 'Murgi Thali', 'Rice, Chicken curry, Cashewnut Salad', '1:1', 150.00, 'Bengali'),
 (2, 'Vorta Thali', 'Aloo Vorta, Shutki Vorta, Potol Vorta, Begoon Vorta, Dim Vorta, Morich Vorta', '1:1', 100.00, 'Bengali'),
 (3, 'Indian Thali', 'Dosa, Sambar, Rayta, Pickle', '1:1', 120.00, 'Indian'),
@@ -86,13 +88,28 @@ INSERT INTO `meal` (`Meal_ID`, `Name`, `Description`, `Proportion`, `Pricing Lis
 --
 
 CREATE TABLE `orders` (
-  `OrderID` int(10) NOT NULL,
+  `OrderID` int(11) NOT NULL,
   `Customer_ID` int(10) NOT NULL,
   `Cost` decimal(10,2) NOT NULL,
   `Status` enum('Pending','On the way','Accepted','Delivered','Cancelled') NOT NULL DEFAULT 'Pending',
   `Date` date NOT NULL,
   `Catering Service` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderID`, `Customer_ID`, `Cost`, `Status`, `Date`, `Catering Service`) VALUES
+(1, 1, 300.00, 'Pending', '2025-09-01', 0),
+(2, 1, 200.00, 'Pending', '2025-09-01', 0),
+(3, 1, 120.00, 'Pending', '2025-09-01', 0),
+(4, 1, 270.00, 'Pending', '2025-09-01', 0),
+(5, 1, 150.00, 'Pending', '2025-09-01', 0),
+(6, 1004, 350.00, 'Pending', '2025-09-02', 0),
+(7, 1004, 100.00, 'Pending', '2025-09-02', 0),
+(8, 1004, 300.00, 'Pending', '2025-09-02', 0),
+(9, 1004, 300.00, 'Pending', '2025-09-02', 0);
 
 -- --------------------------------------------------------
 
@@ -103,9 +120,25 @@ CREATE TABLE `orders` (
 CREATE TABLE `orders_have_meal` (
   `M_ID` int(10) NOT NULL,
   `OrderID` int(10) NOT NULL,
-  `Price` decimal(10,2) NOT NULL,
-  `Quantity` int(11) NOT NULL DEFAULT 1
+  `Quantity` int(11) NOT NULL DEFAULT 1,
+  `Price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders_have_meal`
+--
+
+INSERT INTO `orders_have_meal` (`M_ID`, `OrderID`, `Quantity`, `Price`) VALUES
+(1, 4, 1, 150.00),
+(1, 5, 1, 150.00),
+(1, 6, 1, 150.00),
+(2, 6, 2, 100.00),
+(2, 7, 1, 100.00),
+(3, 3, 1, 120.00),
+(3, 4, 1, 120.00),
+(4, 2, 1, 200.00),
+(5, 8, 1, 300.00),
+(6, 9, 1, 300.00);
 
 -- --------------------------------------------------------
 
@@ -130,13 +163,11 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`U_ID`, `Email`, `Exp_Years`, `Name`, `Address`, `Type`, `Password`) VALUES
 (1, 'mehjabin.hasan@gmail.com', 11, 'Mehjabin Hasan', '3/B Selpark, Mirpur', 'Cook', 'mou1111'),
 (2, 'araf.cooker@gmail.com', 3, 'Araf Rakib', 'Sec-4, Uttara, Dhaka', 'Cook', 'araf4200'),
-(3, 'cr7@gmail.com', 0, 'cr7', 'lisbon, portugal', 'Cook', 'cr71234'),
 (101, 'admin101@gmail.com', 0, 'Admin01', '34/d Baily Road, Dhaka', 'Admin', 'admin123'),
 (1001, 'farhan.zahin@gmail.com', 0, 'Farhan Zahin', '7/A Banasree, Dhaka', 'Customer', 'fz1234'),
 (1002, 'ahona.hasan@gmail.com', 0, 'Ahona Hasan', '33/c Banani, Dhaka', 'Customer', 'ahona1234'),
 (1003, 'jungkook@gmail.com', 0, 'Jung Kook', '16/f Gulshan-1, Dhaka', 'Customer', 'kook97'),
-(1004, 'messi@gmail.com', 0, 'messi', 'argentina, rosario', 'Customer', '123456'),
-(1005, 'fz2008@gmail.com', 0, 'zayma', 'banasree, dhaka', 'Customer', 'fz2008');
+(1004, 'farzana.eti@gmail.com', 0, 'Farzana Eti', 'mirpur', 'Customer', '1234');
 
 --
 -- Triggers `user`
@@ -202,8 +233,8 @@ CREATE TABLE `user_id_tracker` (
 --
 
 INSERT INTO `user_id_tracker` (`user_type`, `next_id`) VALUES
-('Cook', 4),
-('Customer', 1006),
+('Cook', 3),
+('Customer', 1005),
 ('Admin', 102);
 
 -- --------------------------------------------------------
@@ -222,12 +253,9 @@ CREATE TABLE `user_phone_no` (
 --
 
 INSERT INTO `user_phone_no` (`User_ID`, `Phone_No`) VALUES
-(3, 1752537282),
 (1001, 1552306466),
 (1002, 1316733425),
-(1003, 1635895385),
-(1004, 1652537282),
-(1005, 1762537282);
+(1003, 1635895385);
 
 --
 -- Indexes for dumped tables
@@ -241,11 +269,12 @@ ALTER TABLE `complaint_support`
   ADD KEY `user_complaint_fk` (`User_ID`);
 
 --
--- Indexes for table `customer_rates_cook`
+-- Indexes for table `customer_rates_cooks`
 --
-ALTER TABLE `customer_rates_cook`
-  ADD PRIMARY KEY (`Customer_ID`,`Cook_ID`),
-  ADD KEY `cook_rate_fk` (`Cook_ID`);
+ALTER TABLE `customer_rates_cooks`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `cook_rate_fk` (`CookID`),
+  ADD KEY `customer_rate_fk` (`CustomerID`);
 
 --
 -- Indexes for table `meal`
@@ -298,10 +327,16 @@ ALTER TABLE `user_phone_no`
 --
 
 --
+-- AUTO_INCREMENT for table `customer_rates_cooks`
+--
+ALTER TABLE `customer_rates_cooks`
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `OrderID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
@@ -314,11 +349,11 @@ ALTER TABLE `complaint_support`
   ADD CONSTRAINT `user_complaint_fk` FOREIGN KEY (`User_ID`) REFERENCES `user` (`U_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `customer_rates_cook`
+-- Constraints for table `customer_rates_cooks`
 --
-ALTER TABLE `customer_rates_cook`
-  ADD CONSTRAINT `cook_rate_fk` FOREIGN KEY (`Cook_ID`) REFERENCES `user` (`U_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `customer_rate_fk` FOREIGN KEY (`Customer_ID`) REFERENCES `user` (`U_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `customer_rates_cooks`
+  ADD CONSTRAINT `cook_rate_fk` FOREIGN KEY (`CookID`) REFERENCES `user` (`U_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `customer_rate_fk` FOREIGN KEY (`CustomerID`) REFERENCES `user` (`U_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `orders`
